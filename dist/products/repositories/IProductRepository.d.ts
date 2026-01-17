620f5f2feac6746b579507a264b32d41d99370d9
P3ND4 <106392394+P3ND4@users.noreply.github.com>
2026-01-17 00:33:59 -0500

    build

---
+++ dist/products/repositories/IProductRepository.d.ts
@@ -0,0 +1,10 @@
+import { GenericProduct } from "generated/prisma";
+import { CreateProductDto } from "../dto/create-product.dto";
+import { UpdateProductDto } from "../dto/update-product.dto";
+export interface IProductRepository {
+    findAllProducts(): Promise<GenericProduct[]>;
+    createProduct(data: CreateProductDto): Promise<GenericProduct>;
+    findProductById(id: string): Promise<GenericProduct | null>;
+    updateProduct(id: string, data: UpdateProductDto): Promise<GenericProduct>;
+    deleteProduct(id: string): Promise<GenericProduct>;
+}
